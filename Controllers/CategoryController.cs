a6a946c70657a1b9e3b06154a77dd8dc7e44fee0
Sherajul-Shakil <sherajulshakil14@cse.pstu.ac.bd>
2024-10-20 13:43:34 +0600

    Category CRUD operations

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,127 @@
+﻿// using BulkyWeb.DataAccess.Repository.IRepository;
+using BulkyWeb.Data;
+using BulkyWeb.Models;
+// using BulkyWeb.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+	// [Area("Admin")]
+	// [Authorize(Roles = SD.Role_Admin)]
+	public class CategoryController : Controller
+	{
+		// private readonly IUnitOfWork _unitOfWork;
+		// public CategoryController(IUnitOfWork unitOfWork)
+		// {
+		// 	_unitOfWork = unitOfWork;
+		// }
+
+		private readonly ApplicationDbContext _db;
+		public CategoryController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+		public IActionResult Index()
+		{
+			// List<Category> objCategoryList = _unitOfWork.Category.GetAll().ToList();
+			// return View(objCategoryList);
+			List<Category> objCategoryList = _db.Categories.ToList();
+			return View(objCategoryList);
+		}
+
+		public IActionResult Create()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Create(Category obj)
+		{
+			if (obj.Name == obj.DisplayOrder.ToString())
+			{
+				ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+			}
+
+			if (ModelState.IsValid)
+			{
+				// _unitOfWork.Category.Add(obj);
+				// _unitOfWork.Save();
+				_db.Categories.Add(obj);
+				_db.SaveChanges();
+				TempData["success"] = "Category created successfully";
+				return RedirectToAction("Index");
+			}
+			return View();
+
+		}
+
+		public IActionResult Edit(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category? categoryFromDb = _db.Categories.Find(id);
+			// Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
+			// Category? categoryFromDb2 = _db.Categories.Where (u => u.Id == id).FirstOrDefault();
+
+			if (categoryFromDb == null)
+			{
+				return NotFound();
+			}
+			return View(categoryFromDb);
+		}
+		[HttpPost]
+		public IActionResult Edit(Category obj)
+		{
+			if (obj.Name == obj.DisplayOrder.ToString())
+			{
+				ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+			}
+			if (ModelState.IsValid)
+			{
+				// _unitOfWork.Category.Update(obj);
+				// _unitOfWork.Save();
+				_db.Categories.Update(obj);
+				_db.SaveChanges();
+				TempData["success"] = "Category updated successfully";
+				return RedirectToAction("Index");
+			}
+			return View();
+
+		}
+
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			// Category? categoryFromDb = _unitOfWork.Category.Get(u => u.Id == id);
+			Category? categoryFromDb = _db.Categories.Find(id);
+
+			if (categoryFromDb == null)
+			{
+				return NotFound();
+			}
+			return View(categoryFromDb);
+		}
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeletePOST(int? id)
+		{
+			// Category? obj = _unitOfWork.Category.Get(u => u.Id == id);
+			Category? obj = _db.Categories.Find(id);
+			if (obj == null)
+			{
+				return NotFound();
+			}
+			// _unitOfWork.Category.Remove(obj);
+			// _unitOfWork.Save();
+			_db.Categories.Remove(obj);
+			_db.SaveChanges();
+			TempData["success"] = "Category deleted successfully";
+			return RedirectToAction("Index");
+		}
+
+	}
+}
