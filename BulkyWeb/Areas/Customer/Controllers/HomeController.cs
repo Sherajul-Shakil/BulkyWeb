2a98e261fb7764c5bdbc7d80af679693ff73915b
Sherajul-Shakil <sherajulshakil14@cse.pstu.ac.bd>
2024-10-22 12:23:22 +0600

    Add product list and details to home page

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,32 +1,93 @@
-using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
+using BulkyWeb.Repository.IRepository;
 using BulkyWeb.Models;
+using BulkyWeb.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
-namespace BulkyWeb.Areas.Customer.Controllers;
-
-[Area("Customer")]
-public class HomeController : Controller
+namespace BulkyBookWeb.Areas.Customer.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    [Area("Customer")]
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        public IActionResult Index()
+        {
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Details(int productId)
+        {
+            Product product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category");
+            return View(product);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//using System.Diagnostics;
+//using Microsoft.AspNetCore.Mvc;
+//using BulkyWeb.Models;
+
+//namespace BulkyWeb.Areas.Customer.Controllers;
+
+//[Area("Customer")]
+//public class HomeController : Controller
+//{
+//    private readonly ILogger<HomeController> _logger;
+
+//    public HomeController(ILogger<HomeController> logger)
+//    {
+//        _logger = logger;
+//    }
+
+//    public IActionResult Index()
+//    {
+//        return View();
+//    }
+
+//    public IActionResult Privacy()
+//    {
+//        return View();
+//    }
+
+//    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+//    public IActionResult Error()
+//    {
+//        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+//    }
+//}
+
+
